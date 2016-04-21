de744925e098d6ba37cd32ae42e1d3c8856cce22
Telmo Barros <telmo.20@hotmail.com>
2016-04-21 01:52:32 +0100

    changed Poi to POI

---
+++ src/POI.cpp
@@ -0,0 +1,26 @@
+/*
+ * Poi.cpp
+ *
+ *  Created on: Apr 21, 2016
+ *      Author: Antonio
+ */
+
+#include "POI.h"
+
+POI::POI(int id, POIType type) {
+	this->nodeID = id;
+	this->type = type;
+}
+
+POI::~POI() {
+	// TODO Auto-generated destructor stub
+}
+
+const int POI::getNodeID() const{
+	return nodeID;
+}
+
+const POIType POI::getType() const{
+		return type;
+}
+
---
+++ src/POI.h
@@ -0,0 +1,25 @@
+/*
+ * Poi.h
+ *
+ *  Created on: Apr 21, 2016
+ *      Author: Antonio
+ */
+
+#ifndef SRC_POI_H_
+#define SRC_POI_H_
+
+#include "Node.h"
+
+enum POIType{POMPGAS,RESTAURANT,HOTEL};
+
+class POI {
+	int  nodeID;
+	POIType type;
+public:
+	POI(int id, POIType type);
+	virtual ~POI();
+	const int getNodeID() const;
+	const POIType getType() const;
+};
+
+#endif /* SRC_POI_H_ */
