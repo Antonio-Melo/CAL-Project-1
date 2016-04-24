d7ba98f58b6982e0ca9b779b192764cf91224638
Jorge <jorge-vale1@hotmail.com>
2016-04-24 17:04:27 +0100

    road.h documentado

--- src/Road.h
+++ src/Road.h
@@ -12,9 +12,13 @@
 #include "Node.h"
 
 using namespace std;
-
+/**
+ * enum used to represent a type of a road
+ */
 enum roadType{HIGHWAY,NATIONAL,ROUTE};
-
+/**
+ *
+ */
 class Road {
 private:
 	string name;
@@ -23,15 +27,55 @@ private:
 	vector<int> nodesID;
 	double price;
 public:
+	/**
+	 * Road constructor to generate roads
+	 * @param string name that represents name of the road, bool is that represents if the road is two ways or not, and the roadType to represent the type of the road
+	 */
 	Road(string name,bool is,roadType type);
+	/**
+	 * Adds a id from a node to the vector
+	 * @param id ID to be added
+	 */
 	void addNodeID(int id);
+	/**
+	 * Checks if the road is two ways
+	 * @return if the road is two ways or not
+	 */
 	bool isIsTwoWay() const;
+	/**
+	 * Returns name of road
+	 * @return string with the name of the road
+	 */
 	const string& getName() const;
+	/**
+	 * Returns nodes ID
+	 * @return vector with all ID of the nodes in road
+	 */
 	const vector<int> getNodesID() const;
+	/**
+	 * Returns type of road
+	 * @return string with the type of the road
+	 */
 	const roadType getType()const;
+	/**
+	 * Returns price of road
+	 * @return double with the price of the road
+	 */
 	const double getPrice() const;
+	/**
+	 * Sets name to a road
+	 * @param name new name to set
+	 */
 	void setName(const string& name);
+	/**
+	 * Sets if a road is two ways
+	 * @param isTwoWay new is_two_way to set
+	 */
 	void setIsTwoWay(bool isTwoWay);
+	/**
+	 * Sets nodesID to a new vector
+	 * @param nodesID New vector to set
+	 */
 	void setNodesID(const vector<int> nodesID);
 };
 
